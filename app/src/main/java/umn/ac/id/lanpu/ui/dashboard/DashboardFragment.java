0253a35c21383efec241259d3b143ada4aff316e
AlfredPros <alfredkuhlman@yahoo.co.id>
2022-12-04 17:15:35 +0700

    Update DashboardFragment.java

--- app/src/main/java/umn/ac/id/lanpu/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/umn/ac/id/lanpu/ui/dashboard/DashboardFragment.java
@@ -6,13 +6,11 @@ import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
