71ca7000187ab77052e8dc63b72d84cc277e7f81
IsRylo <91057829+IsRylo@users.noreply.github.com>
2022-12-05 05:30:38 +0800

    Passsword Change

--- app/src/main/java/umn/ac/id/lanpu/ui/notifications/ChangePassword.java
+++ app/src/main/java/umn/ac/id/lanpu/ui/notifications/ChangePassword.java
@@ -14,7 +14,10 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 
 import umn.ac.id.lanpu.R;
@@ -26,6 +29,7 @@ public class ChangePassword extends AppCompatActivity {
     EditText input;
     EditText inputConf;
     Button submit;
+    String email;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,27 +46,27 @@ public class ChangePassword extends AppCompatActivity {
             public void onChanged(@NonNull DataSnapshot dataSnapshot) {
                 String name = dataSnapshot.child("name").getValue(String.class);
                 accName.setText(name);
+                email = dataSnapshot.child("email").getValue(String.class);
             }
         });
 
-        input = findViewById(R.id.input_password);
-        inputConf = findViewById(R.id.confirm_password);
         submit = findViewById(R.id.change_password_button);
 
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String pass = input.getText().toString();
-                String passConf = inputConf.getText().toString();
+                FirebaseAuth auth = FirebaseAuth.getInstance();
 
-                if (pass.equals(passConf) && !pass.equals("") && !passConf.equals("")) {
-                    accName.setText("Correct!");
-                    Toast.makeText(ChangePassword.this, "Password has been changed.", Toast.LENGTH_SHORT).show();
-                    finish();
-                }
-                else {
-                    Toast.makeText(ChangePassword.this, "Password is not confirmed.", Toast.LENGTH_SHORT).show();
-                }
+
+                auth.sendPasswordResetEmail(email)
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(ChangePassword.this, "Please Check Your Email", Toast.LENGTH_LONG).show();
+                                }
+                            }
+                        });
             }
         });
 
