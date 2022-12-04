18ce064724ffb6a6daca7fa153db4759843783f2
MeitioS <101271548+MeitioS@users.noreply.github.com>
2022-12-04 22:27:38 +0700

    dashboard

--- app/src/main/java/umn/ac/id/lanpu/ui/dashboard/DashboardViewModel.java
+++ app/src/main/java/umn/ac/id/lanpu/ui/dashboard/DashboardViewModel.java
@@ -1,5 +1,15 @@
 package umn.ac.id.lanpu.ui.dashboard;
 
+<<<<<<< Updated upstream
+=======
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.Objects;
+
+>>>>>>> Stashed changes
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
@@ -16,4 +26,28 @@ public class DashboardViewModel extends ViewModel {
     public LiveData<String> getText() {
         return mText;
     }
+<<<<<<< Updated upstream
+=======
+
+    public LiveData<Long> getDurationLiveDate() {
+        return duration;
+    }
+
+    // TODO: Create FUNCTION that RETURNS DURATION as STRING time format
+    //  Return 00 days 00 minutes 00 seconds
+    //  public String getDurationTime(long duration)
+
+    public String getDurationTime () {
+        dateTime = thisDuration.addTo(dateTime);
+        dateTime = dateTime.plus(thisDuration);
+    }
+
+    // TODO: Create BACKGROUND PROCESS THAT UPDATES THE DURATION IN MILLISECONDS
+    //  Buatlah sebuah background process yang akan menhitung seconds ketika checkedIN berubah menjadi true
+    //  Simple aja buatnya cuma counter
+    //  public void startCounter()
+
+    public void startCounter() {}
+
+>>>>>>> Stashed changes
 }
